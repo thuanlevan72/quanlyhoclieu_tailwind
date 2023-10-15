71b8c1e0a538a8d879f0fb4636394a0df24a7072
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-10-15 09:08:56 +0700

    first commit

---
+++ src/i18n/config.js
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
+  lng: 'en',
+  resources: {
+    en: {
+      translations: require('./localization/en/translation.json'),
+    },
+    esp: {
+      translations: require('./localization/esp/translation.json'),
+    },
+    ar: {
+      translations: require('./localization/ar/translation.json'),
+    },
+    vn: {
+      translations: require('./localization/vn/translation.json'),
+    },
+  },
+  ns: ['translations'],
+  defaultNS: 'translations',
+});
+
+i18n.languages = ['en', 'esp', 'ar', 'vn'];
+
+export default i18n;
